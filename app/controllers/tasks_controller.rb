393009db3fa1d4942390268ab5ce059f6914d3bf
Apple chen <appletabby@gmail.com>
2019-11-05 08:39:28 +0800

    Add create method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,4 +6,20 @@ class TasksController < ApplicationController
   def new
     @task = Task.new
   end
+
+  def create
+    @task = Task.new(task_params)
+
+    if @task.save
+      redirect_to tasks_path, notice: "新增任務成功！"
+    else
+      render :new
+    end
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:content, :priority, :status, :started_at, :ended_at)
+  end
+
 end
