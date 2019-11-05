aa55782c7e12c6e73dee60c5cd5585a8976533c1
Apple chen <appletabby@gmail.com>
2019-11-05 10:17:07 +0800

    Write priority & status validations(WIP)

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,8 @@
 class Task < ApplicationRecord
     enum priority: { high: 1, medium: 2, low: 3 }
     enum status: { to_do: 1, in_progress: 2, done: 3 }
+    # validates :priority, inclusion: { in: %w(high medium low),
+    #   message: "%{value} is not a valid priority." }
+    # validates :status, inclusion: { in: %w(to_do in_progress done),
+    #   message: "%{value} is not a valid status." }
 end
