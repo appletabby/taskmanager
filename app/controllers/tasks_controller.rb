89f477e6eed88a6a95a53a83c9a311fc76776098
Apple chen <appletabby@gmail.com>
2019-11-05 09:42:03 +0800

    Add update method

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -21,6 +21,16 @@ class TasksController < ApplicationController
     @task = Task.find_by(id: params[:id])  
   end
 
+  def update
+    @task = Task.find_by(id: params[:id])
+
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "資料更新成功！"
+    else
+      render :edit
+    end
+  end
+
   private
   def task_params
     params.require(:task).permit(:content, :priority, :status, :started_at, :ended_at)
