d99a036cd879531df37b2a29f28cdbda0863d842
Apple chen <appletabby@gmail.com>
2019-11-16 16:32:37 +0800

    Modify View Model Controller & Provide translation

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,6 @@
 class Task < ApplicationRecord
-    enum priority: { high: 1, medium: 2, low: 3 }
-    enum status: { to_do: 1, in_progress: 2, done: 3 }
+    enum priority: { I18n.t('model.priority.high')=> 1, I18n.t('model.priority.medium')=> 2, I18n.t('model.priority.low')=> 3 }
+    enum status: { I18n.t('model.status.to_do')=> 1, I18n.t('model.status.in_progress')=> 2, I18n.t('model.status.done')=> 3 }
     # validates :priority, inclusion: { in: %w(high medium low),
     #   message: "%{value} is not a valid priority." }
     # validates :status, inclusion: { in: %w(to_do in_progress done),
