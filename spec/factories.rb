96c4a336250027feec5507ffff478d505ce2dc07
Apple chen <appletabby@gmail.com>
2019-11-20 11:34:39 +0800

    Modify priority and status to dropdown style & remove unnecessary i18n

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :task do
     sequence(:content) { |n| "task content #{n}" }
-    sequence(:priority) { |n| I18n.t('model.priority.high') || I18n.t('model.priority.medium') || I18n.t('model.priority.low')}
+    sequence(:priority) { |n| "high" || "medium" || "low"}
   end	  
 end
